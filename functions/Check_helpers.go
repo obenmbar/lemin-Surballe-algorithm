6787f85dee50684806363136086f4a1e45df9c76
Othmane  Benmbarek <obenmbar@noreply.learn.zone01oujda.ma>
2025-11-11 14:31:33 +0000

    Update functions/Check_helpers.go

--- functions/Check_helpers.go
+++ functions/Check_helpers.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"strings"
 )
-
-// handleSpecialComment processes '##start' or '##end' lines and links them to the right room.
+// handleSpecialComment processes '##start' or '%23%23end' lines and links them to the right room.
 func handleSpecialComment(lines []string, idx *int, farm *Farm, foundTunnels *bool, specialType string) error {
+	var Index_start int
 	if *idx+1 >= len(lines) {
 		return fmt.Errorf("'%s' must not be the last line", specialType)
 	}
-
-	nextLine := strings.TrimSpace(lines[*idx+1])
+	for v := *idx + 1; v < len(lines); v++ {
+		value_after_end_start:= strings.TrimSpace(lines[v])
+		if value_after_end_start == "" || strings.HasPrefix(value_after_end_start, "#") {
+			if strings.TrimSpace(lines[v]) != "##start" &&  strings.TrimSpace(lines[v]) != "##end"{
+				Index_start += 1
+				continue
+			} else {
+				return fmt.Errorf("error il ya la valeur ##start directement desous de la valeur ##end ou le contraire ")
+			}
+		} else {
+			break
+		}
+	}
+	nextLine := strings.TrimSpace(lines[*idx+Index_start+1])
 
 	if msg, ok := isRoomOrTunnel(nextLine, farm, foundTunnels); !ok {
 		return fmt.Errorf("after '%s' the next line must be a valid room: %s", specialType, msg)
@@ -34,12 +46,11 @@ func handleSpecialComment(lines []string, idx *int, farm *Farm, foundTunnels *bo
 		farm.SpecialRooms["end"] = name
 	}
 
-	*idx++
+	*idx += Index_start+1
 	return nil
 }
-
 // isRoomOrTunnel checks if a given line defines a room, tunnel, or something invalid.
-func isRoomOrTunnel(line string, farm *Farm,  foundTunnel *bool) (string, bool) {
+func isRoomOrTunnel(line string, farm *Farm, foundTunnel *bool) (string, bool) {
 	if isEmpty(line) {
 		return "line is empty", false
 	}
